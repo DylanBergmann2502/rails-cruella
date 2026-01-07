5a12d8653867788857cf61581d18abf5c6a8d623
Dylan <locducnguyen2001@gmail.com>
2026-01-07 17:56:48 +0700

    fix: update to use new Pagy::Method class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 # app/controllers/application_controller.rb
 class ApplicationController < ActionController::API
-  include Pagy::Backend
+  include Pagy::Method
   include Pundit::Authorization
 
   private
